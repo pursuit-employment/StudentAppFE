125bdd437e82f9e2a55f687481f1b034d81432c6
DestinyJoyner <destinyjoyner@pursuit.org>
2023-07-14 01:01:16 -0400

    cleaned up searchFilter files

--- src/components/SearchFilter/SearchFilter.js
+++ src/components/SearchFilter/SearchFilter.js
@@ -5,47 +5,39 @@ function SearchFilter({ setFunction, students }) {
   const [searchInput, setSearchInput] = useState("");
 
   function handleSearchBar(e) {
-    const searchValue= e.target.value;
+    const searchValue = e.target.value;
     setSearchInput(searchValue);
-// More descriptive step by step labeling and variables
+
     if (!searchValue) {
       setFunction(students);
     } else {
-        // const splitSearchValue = searchValue.split(" ")
-        const onlyLettersVal = searchValue
-        .split("")
-        .filter((el) => /[a-z]/gi.test(el))
-        .join("");
-      // filters out symbols for val
-    //   const onlyLettersVal = searchValue
-    //     .split("")
-    //     .filter((el) => /[a-z]/gi.test(el))
-    //     .join("");
-      filterStudents(onlyLettersVal, students, setFunction);
+      const searchInputValuesArr = searchValue.toLowerCase().split(" ");
+      filterStudents(searchInputValuesArr, students, setFunction);
     }
   }
 
-  // Handle Filter function
-  function filterStudents(val, arr, setFunction) {
-    const filteredStudents = arr.filter((el) => {
-      const first = el.firstName;
-      const last = el.lastName;
-      const regex = new RegExp(val, "gi");
-
-      if (regex.test(first) || regex.test(last) || regex.test(`${first}${last}`)) {
-        return el;
+  // Handle Student Filter function
+  function filterStudents(inputArr, studentArr, setFunction) {
+    const filteredStudents = studentArr.filter((student) => {
+      const first = student.firstName;
+      const last = student.lastName;
+      const fullName = `${first} ${last}`.toLowerCase();
+      const fullNameIncludesSearchValue = inputArr.every((el) =>
+        fullName.includes(el)
+      );
+      if (fullNameIncludesSearchValue) {
+        return student;
       }
-      
     });
     setFunction(filteredStudents);
   }
 
   return (
-    <label className="search-filter">
+    <label className="searchBar">
       <input
         type="text"
         placeholder="Search by name"
-        className="search-filter-searchbar"
+        className="searchBar_input"
         onChange={(event) => handleSearchBar(event)}
       />
     </label>
