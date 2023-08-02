64f284699078d1c066dcf382a3d893855e68785d
werner33 <jordanmanley@JORDANs-MacBook-Pro.local>
2023-08-02 14:05:04 -0400

    set states on student cards

--- src/components/studentCardList/StudentCardList.js
+++ src/components/studentCardList/StudentCardList.js
@@ -8,25 +8,44 @@ const StudentCardList = () => {
 
     // set hook for student data 
     const [students, setStudents] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
-        // fetch data from https://api.hatchways.io/assessment/students
+        setLoading(true);
         fetch('https://api.hatchways.io/assessment/students')
         .then(response => response.json())
         .then(data => {
-            // update hook with data
-            setStudents(data.students);
+            setTimeout(function(){
+                if(true || data.status === 403){
+                    throw {status: 'error', message: 'An error occurred.'}
+                } else {
+                    setError(false);
+                    setStudents(data.students);
+                    setLoading(false);
+                }
+            }, 2000)
+            
+        }).catch(error => {
+            setError(error);
+            setLoading(false)
         })
     }, []);
     
 
     return (
-        <div className="studentCardList">
-            {/* map through data  */}
-            {students.map(student => {
+        <div className={`studentCardList ${error &&  'studentCardList--error'}`}>
+            {!loading && students.length > 0 && !error && students.map(student => {
                 // render a student card for every student
                 return (<StudentCard student={student} />)
             })}
+
+            {loading && <div className="studentCardList__empty">Loading...</div>}
+
+            {!loading && !error && students.length === 0 && <div className="studentCardList__empty">No results : (</div>}
+
+             {!loading && error && <div className="studentCardList__empty">{error.message}</div>}
         </div>
     )
 }
