7b95d2d941572089dd888fee6741fb65d585ce03
werner33 <jordanmanley@JORDANs-MacBook-Pro.local>
2023-08-08 13:44:27 -0400

    build responsive navbar"

--- src/layout/navbar/Navbar.js
+++ src/layout/navbar/Navbar.js
@@ -1,25 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom';
 
 import './Navbar.scss';
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <div className="navbar__container">
-            <div className="navbar__logo"> 
-                <Link to="">Student App</Link>
-            </div>
-            <div className="navbar__items">
-                <ul>
-                    <Link to="/"><li>Students</li></Link>
-                    <Link to="contact"><li>Contact</li></Link>
-                    <Link to="about"><li>About</li></Link>
-                </ul>
+
+    const [toggleShow, setToggleShow] = useState(false);
+
+    const toggleNavBarTest = () => {
+        console.log('hambiurger menu clicked!')
+        setToggleShow(!toggleShow);
+    }
+
+    return (
+        <div className="navbar">
+            <div className="navbar__container">
+                <div className="navbar__logo" onClick={()=> setToggleShow(false)}> 
+                    <Link to="">Student App</Link>
+                </div>
+                <div className={"navbar__items " + (toggleShow && 'navbar__items--show')}>
+                    <ul onClick={()=> toggleNavBarTest()}>
+                        <Link to="/"><li>Students</li></Link>
+                        <Link to="contact"><li>Contact</li></Link>
+                        <Link to="about"><li>About</li></Link>
+                    </ul>
+                </div>
+
+                <div 
+                    className="navbar__hamburger"
+                    onClick={() => toggleNavBarTest()}
+                >
+                    =
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
